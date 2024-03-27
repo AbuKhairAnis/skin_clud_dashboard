97740fba7fad4a139e5fae52ecc6ea9932a3b427
rashadrabby <rashadulislamrabby@gmail.com>
2024-03-28 01:22:24 +0600

    Cleaned

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,15 @@
-import { Inter } from "next/font/google";
-import "./ui/globals.css";
+import './global.css';
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Abu Khair Anis Next.js Admin Dashboard",
-  description: "Next.js 14 Tutorial",
+  title: "Admin Dashboard",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body >{children}</body>
     </html>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
 const Homepage = () => {
   return (
     <div>
-      Homepage
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-5xl font-bold text-center mt-5">Welcome to Admin Panel</h1>
     </div>
   );
 };
