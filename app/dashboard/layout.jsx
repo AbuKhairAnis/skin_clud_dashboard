3d5f3bb9b169711c71ab0370db08ec84c74618c2
rashadrabby <rashadulislamrabby@gmail.com>
2024-03-28 01:08:22 +0600

    Updated

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,18 +1,15 @@
 import Navbar from "../ui/dashboard/navbar/navbar";
 import Sidebar from "../ui/dashboard/sidebar/sidebar";
-import styles from "../ui/dashboard/dashboard.module.css";
-import Footer from "../ui/dashboard/footer/footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <div className={styles.menu}>
+    <div >
+      <div >
         <Sidebar />
       </div>
-      <div className={styles.content}>
+      <div >
         <Navbar />
         {children}
-        <Footer />
       </div>
     </div>
   );
