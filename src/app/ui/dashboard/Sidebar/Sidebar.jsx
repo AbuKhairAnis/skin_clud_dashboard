74984e14c15e42c1a971579f03557e371a714b26
AbuKhairAnis <abukhairanis123@gmail.com>
2024-03-27 23:32:22 +0600

    3th Commit

--- src/app/ui/dashboard/Sidebar/Sidebar.jsx
+++ src/app/ui/dashboard/Sidebar/Sidebar.jsx
@@ -4,8 +4,31 @@ const Sidebar = () => {
   return (
     <div>
       <div>
-        <Image src={"/Logo.png"} alt="" width="50" height="50" />
+        {/* <Image src={"/Logo.png"} alt="" width="50" height="50" /> */}
       </div>
+      <ul className="flex flex-col m-10">
+        <li className="mt-2">Overview</li>
+        <li>Profile</li>
+        <li>Appointments</li>
+        <li>
+          History
+          <li>Videos</li>
+          <li>Videos Library </li>
+        </li>
+        <li>Messages</li>
+        <li>
+          Support
+          <li>FAQ</li>
+          <li>Procedure Information </li>
+        </li>
+        <li>
+          Aftercare
+          <li>Quizzes</li>
+          <li>Health & Skin Assesment</li>
+        </li>
+        <li>Finances</li>
+        <li>Virtual Consultation</li>
+      </ul>
     </div>
   );
 };
