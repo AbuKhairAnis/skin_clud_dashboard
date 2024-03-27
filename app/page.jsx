0272a6ba191159aee2ebd822e58adcc5537c187e
AbuKhairAnis <abukhairanis123@gmail.com>
2024-03-28 00:20:14 +0600

    new Project

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+  return (
+    <div>
+      Homepage
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Homepage;
--- src/app/ui/dashboard/Titlebar/TItlebar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TItlebar = () => {
-  return (
-    <div>TItlebar</div>
-  )
-}
-
-export default TItlebar
